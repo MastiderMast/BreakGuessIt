08d8afb12d2ced46586559f8eaf6451305e6b923
Masti <65887954+MastiderMast@users.noreply.github.com>
2021-11-01 00:01:04 +0100

    Add irregular timeouts

--- index.js
+++ index.js
@@ -3,18 +3,26 @@ const sendkeys = require('sendkeys')
 loop();
 var i = 0
 function loop() {
-  setTimeout(function() {
-    console.log(i);
-    
-    if (i <= 10) {
-      loop();
-      sendkeys.sync(`{ENTER}`)
-      sendkeys.sync(`${i}`)
-    }else {
-        console.log("DONE")
-    }
+
+    var wait = randomIntFromInterval(486, 2623)
+    console.log(wait)
+
+    setTimeout(function() {
+
+        console.log(i);
+        
+        if (i <= 10) {
+          loop();
+          sendkeys.sync(`{ENTER}`)
+          sendkeys.sync(`${i}`)
+        }else {
+            console.log("DONE")
+        }
 
     i++;
+  }, wait)
+}
 
-  }, 3000)
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
