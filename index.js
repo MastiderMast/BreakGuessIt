075642ff222d10975a78326346b5d945a9d972d2
TomatoCake <60300461+DEVTomatoCake@users.noreply.github.com>
2021-12-22 21:15:57 +0100

    FIXÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄN

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
 const sendkeys = require('sendkeys')
-const { red, green, blue, yellow, cyan, greenBright, redBright, grey, yellowBright, cyanBright, black, blueBright } = require('chalk');
+const { green, blue, yellow } = require('chalk')
 
 console.clear()
-loop();
 var start = 99
 var end = 1000
 
-console.log(`${yellow("[BreakGuessIt]")} ${green(`Start counting from ${start} to ${end}`)}`);
+console.log(`${yellow("[BreakGuessIt]")} ${green(`Start counting from ${start} to ${end}`)}`)
 
 function loop() {
-
     var wait = randomIntFromInterval(486, 3623)
 
     setTimeout(function() {
-      if (start <= end) {
-        loop();
-        sendkeys.sync(`${start}`)
-        sendkeys.sync(`{ENTER}`)
-        console.log(`${yellow("[BreakGuessIt]")} ${green(`${start+"/"+end}`)} | ${blue(`Timeout: ${wait}`)}`);
-      }else {
-        console.log(`${yellow("[BreakGuessIt]")} ${green(`Done!`)}`);
-      }
+        if (start <= end) {
+            loop()
+            sendkeys.sync(`${start}`)
+            sendkeys.sync(`{ENTER}`)
+            console.log(`${yellow("[BreakGuessIt]")} ${green(`${start+"/"+end}`)} | ${blue(`Timeout: ${wait}`)}`)
+        } else {
+            console.log(`${yellow("[BreakGuessIt]")} ${green(`Done!`)}`)
+        }
 
-      start++;
-  }, wait)
+        start++
+    }, wait)
 }
+loop()
 
 function randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
